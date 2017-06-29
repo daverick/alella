91ac0356d6c4eb5d1eeb748dcea0d84eb1af355a
daverick <daverick@users.noreply.github.com>
2017-06-29 16:08:55 +0200

    change print to be python3 compatible

--- google cloud/getCredentialsFromFile.py
+++ google cloud/getCredentialsFromFile.py
@@ -5,11 +5,11 @@ def getCredentials():
     from  oauth2client import file
     import httplib2
     import ipywidgets as widgets
-    print "Getting the credentials from file..."
+    print("Getting the credentials from file...")
     storage = file.Storage("oauth2.dat")
     credentials=storage.get()
     if credentials is None or credentials.invalid:
-        print '❗'
+        print( '❗')
         display(widgets.Valid(
             value=False,
             description='Credentials are ',
@@ -17,5 +17,5 @@ def getCredentials():
         display(widgets.HTML('go create a credential valid file here: <a target="_blank" href="cloud.google.auth.ipynb.ipynb">gcloud authorization notebook</a> and try again'))
     else:
         http_auth = credentials.authorize(httplib2.Http())
-        print '✅ Ok'
+        print('✅ Ok')
         return credentials
